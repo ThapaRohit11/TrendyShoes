67e525582e3fb5dc5e9eef5f084225d47240cb7b
thaprohit <thaparohit155@gmail.com>
2026-07-28 02:20:11 +0545

    Add public layout and navigation

---
+++ frontend/src/components/public/AppLayout.jsx
@@ -0,0 +1,13 @@
+import Navbar from './Navbar'
+import { Outlet } from 'react-router-dom'
+
+function AppLayout() {
+  return (
+    <div>
+        <Navbar/>
+        <Outlet/>
+    </div>
+  )
+}
+
+export default AppLayout
